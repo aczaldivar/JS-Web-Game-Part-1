59ceddda7b78fd8f642f8caf3b29e5409cc7853e
Anna Zaldivar <anna.zaldivar@gmail.com>
2023-03-29 09:35:18 -0700

    changed function

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ document.body.append(element)
 return element
 }
 
+
+
+function newItem(url, left, bottom){
+    let element = newImage(url, left, bottom)
+
+    element.addEventListener('dblclick', () => {
+        element.remove()
+    })
+}
 newImage('assets/green-Character.gif',200,200)
 newImage('assets/pine-tree.png',450,200)
 newImage('assets/tree.png',200,300)
@@ -16,19 +25,6 @@ newImage('assets/pillar.png',350,100)
 newImage('assets/crate.png',150,200)
 newImage('assets/well.png',500,450)
 
-function newItem(url,left,bottom)
-{
-let element = document.createElement('img')
-element.src = url
-element.style.position = 'fixed'
-element.style.left = `${left}px`
-element.style.bottom = `${bottom}px`
-document.body.append(element)
-
-element.addEventListener('dblclick',function (){
-    element.remove()
-})
-}
 newItem('assets/sheild.png',165,185)
 newItem('assets/staff.png',600,100)
 newItem('assets/sword.png',500,405)
